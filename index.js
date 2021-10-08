3c5c84eb2ef3d36f97b046b9240ecf74c7fe262f
tardyparty <davidctardy@gmail.com>
2021-10-07 19:03:55 -0500

    dotenv fixed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import exchangeRoutes from './routes/exchange.js';
 
@@ -12,6 +13,7 @@ app.use('/exchange', exchangeRoutes);
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+dotenv.config();
 
 
 const CONNECTION_URL = process.env.DB_CONN;
